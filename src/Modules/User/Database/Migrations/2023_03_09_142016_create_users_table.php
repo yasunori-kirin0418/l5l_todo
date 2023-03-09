4f97991b43253f3adb14da99ab41eaa94f398fae
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-03-09 23:35:20 +0900

    Add: usersテーブルの作成

---
+++ src/Modules/User/Database/Migrations/2023_03_09_142016_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->comment('ユーザーID');
+            $table->string('name', 255)->comment('ユーザー名');
+            $table->string('email', 255)->unique()->comment('E-mail');
+            $table->string('password', 255)->comment('パスワード');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
