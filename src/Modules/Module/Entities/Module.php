b853d24688fc4cc701e03298321033a87c838a04
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-03-12 23:46:29 +0900

    Add: modulesテーブル用のモデル作成

---
+++ src/Modules/Module/Entities/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Module\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Module extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+}
