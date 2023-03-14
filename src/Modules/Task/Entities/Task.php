9094ce69e8de41a35af3728dd4d9b8b079c77a27
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-03-15 00:06:19 +0900

    Update: Taskモデル

--- src/Modules/Task/Entities/Task.php
+++ src/Modules/Task/Entities/Task.php
@@ -5,6 +5,7 @@ namespace Modules\Task\Entities;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Task\Database\factories\TaskFactory;
 
 class Task extends Model
 {
@@ -17,8 +18,15 @@ class Task extends Model
     ];
 
     protected $fillable = [
+        'status_id',
+        'user_id',
         'title',
         'description',
         'deadline',
     ];
+
+    protected static function newFactory()
+    {
+        return TaskFactory::new();
+    }
 }
